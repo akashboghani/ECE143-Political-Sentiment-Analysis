cddb6257a3b7937c529e3f7555961279932d8fe8
fruitsnackies <lkyi@ucsd.edu>
2019-05-14 14:16:03 -0700

    fixed so that all tweets are written to csv instead of just one batch

--- collect_data.py
+++ collect_data.py
@@ -1,6 +1,7 @@
 import tweepy
 import json
 import pandas as pd
+import io
 
 with open('twitter_credentials.json') as cred_data:
 	login_info = json.load(cred_data)
@@ -16,12 +17,11 @@ api = tweepy.API(auth)
 all_tweets = []
 twitter_handle = input("Enter twitter handle of user:")
 new_tweets = api.user_timeline(screen_name=twitter_handle, count=200)
-while(len(new_tweets) > 0):
-	tweets_df = pd.read_json(json.dumps(new_tweets[0]._json)) #reads _json property and places in dataframe
-	tweets_df.to_csv('test.csv') #saves to csv file rename to whatever you want
-	all_tweets.extend(new_tweets)
-	last_id = all_tweets[-1].id - 1
-	new_tweets = api.user_timeline(screen_name=twitter_handle, count=200, max_id=last_id)
-	print("######## %d tweets downloaded ########" % len(all_tweets))
-
-	
+with io.open('test.csv','w+',encoding='utf-8') as f_out: 
+	while(len(new_tweets) > 0):
+		tweets_df = pd.read_json(json.dumps(new_tweets[0]._json)) #takes json property data and places in df
+		tweets_df.to_csv(f_out,header=False) #writes df to csv file
+		all_tweets.extend(new_tweets)
+		last_id = all_tweets[-1].id - 1
+		new_tweets = api.user_timeline(screen_name=twitter_handle, count=200, max_id=last_id)
+		print("######## %d tweets downloaded ########" % len(all_tweets))
