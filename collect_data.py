13658c27f0a8ead361c8a604df07167b1209a2e4
akashboghani <akashboghani@gmail.com>
2019-05-15 18:53:39 -0700

    Scraping and storing to csv is now functional.

--- collect_data.py
+++ collect_data.py
@@ -1,5 +1,6 @@
 import tweepy
 import json
+import csv
 
 with open('twitter_credentials.json') as cred_data:
     login_info = json.load(cred_data)
@@ -21,3 +22,16 @@ while(len(new_tweets) > 0):
     new_tweets = api.user_timeline(screen_name=twitter_handle, count=200, max_id=last_id)
     print("######## %d tweets downloaded ########" % len(all_tweets))
 
+outtweets = [[  tweet.id_str, tweet.created_at, tweet.text, tweet.retweet_count, 
+                tweet.lang, tweet.user.id_str, tweet.user.name, tweet.user.screen_name, 
+                tweet.user.followers_count, tweet.user.friends_count, tweet.user.location, 
+                tweet.user.verified ] for tweet in all_tweets]
+
+# writing to the csv file
+
+with open(twitter_handle + '_tweets.csv', 'w', encoding='utf8') as f:
+    writer = csv.writer(f)
+    writer.writerow(['Tweet ID', 'Date Created', 'Tweet', 'Retweets', 'Language', 'User ID', 
+                    'User Name', 'User Twitter Handle', 'Follower Count', 'Friend Count',
+                    'Location', 'Verified'])
+    writer.writerows(outtweets)
